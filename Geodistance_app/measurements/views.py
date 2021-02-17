9a07fde961ae0e6f2035c6ec8b05c9ad91a3ffe8
Lex-23 <Liverpoolsmile23@gmail.com>
2021-02-17 21:12:33 +0300

    add geopy geoip2, make logic view

--- Geodistance_app/measurements/views.py
+++ Geodistance_app/measurements/views.py
@@ -1,17 +1,42 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Measurement
-from  .forms import MeasurementModelForm
+from .forms import MeasurementModelForm
+from geopy.geocoders import Photon
+from geopy.distance import geodesic
+from .utils import get_geo
 
 
 def calculate_distance_view(request):
     obj = get_object_or_404(Measurement, id=1)
     form = MeasurementModelForm(request.POST or None)
+    geolocator = Photon(user_agent='measurements', timeout=10)
+
+    ip = '46.216.43.208'
+    country, city, lat, lon = get_geo(ip)
+    # print('location country', country)
+    # print('location city', city)
+    # print('location lat, lon', lat, lon)
+    location = geolocator.geocode(city)
+    # print('###', location)
+
+    l_lat = lat
+    l_lon = lon
+    pointA = (l_lat, l_lon)
 
     if form.is_valid():
         instance = form.save(commit=False)
-        instance.destination = form.cleaned_data.get('destination')
-        instance.location = 'Minsk'
-        instance.distance = 9000.00
+        destination_ = form.cleaned_data.get('destination')
+        destination = geolocator.geocode(destination_)
+        # print(destination)
+        d_lat = destination.latitude
+        d_lon = destination.longitude
+
+        pointB = (d_lat, d_lon)
+
+        distance = round(geodesic(pointA, pointB).km, 2)
+
+        instance.location = location
+        instance.distance = distance
         instance.save()
 
     context = {
